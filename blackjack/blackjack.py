e8505dde97161ebed0575b34c51ea1ebe113c714
zq99 <9282913+zq99@users.noreply.github.com>
2020-09-26 12:41:21 +0100

    Refactored Blackjack class

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,3 +1,5 @@
+import enum
+
 from blackjack.common import ActionType
 from blackjack.hand import Hand
 from blackjack.score import Score
@@ -6,21 +8,32 @@ from common.util import full_range
 from model.deck import create_game_deck
 
 
+class ResultType(enum.Enum):
+    PlayerWin = 1
+    DealerWin = 2
+    Push = 3
+
+
 class Blackjack:
     def __init__(self, total, decks):
         self.total_rounds = total
         self.deck_count = decks
 
     @staticmethod
-    def __get_dealer_result(dealer, player, deck):
+    def __get_result_against_dealer(dealer, player, deck):
         # dealer issues cards to him/herself until
         # busted or player wins or dealer gets blackjack
         while not dealer.is_busted():
             card = dealer.get_face_down() if dealer.is_cards_hidden() else deck.remove_random_card()
             dealer.add_card(card)
-            if dealer.is_blackjack() or dealer.get_max_total() > player.get_max_total():
-                return True
-        return False
+            if dealer.is_blackjack():
+                if player.is_blackjack():
+                    return ResultType.Push
+                else:
+                    return ResultType.DealerWin
+            elif dealer.get_max_total() > player.get_max_total():
+                return ResultType.DealerWin
+        return ResultType.PlayerWin
 
     @staticmethod
     def __deal_to_player(player, dealer, deck, strategy):
@@ -55,20 +68,16 @@ class Blackjack:
             dealer.add_card(deck.remove_random_card())
             dealer.add_face_down(deck.remove_random_card())
 
-            if player.is_blackjack():
-                score.add_to_player()
+            player = self.__deal_to_player(player, dealer, deck, strategy)
+
+            if player.is_busted():
+                score.add_to_dealer()
             else:
-
-                player = self.__deal_to_player(player, dealer, deck, strategy)
-
-                if player.is_busted():
+                result = self.__get_result_against_dealer(dealer, player, deck)
+                if result == ResultType.DealerWin:
                     score.add_to_dealer()
-                elif not player.is_blackjack():
-                    dealer_result = self.__get_dealer_result(dealer, player, deck)
-                    if dealer_result:
-                        score.add_to_dealer()
-                    else:
-                        score.add_to_player()
-                else:
+                elif result == ResultType.PlayerWin:
                     score.add_to_player()
+                else:
+                    score.add_to_push()
         return score
--- blackjack/score.py
+++ blackjack/score.py
@@ -5,6 +5,7 @@ class Score:
     def __init__(self):
         self.player = 0
         self.dealer = 0
+        self.push = 0
 
     def add_to_player(self):
         self.player += 1
@@ -12,11 +13,14 @@ class Score:
     def add_to_dealer(self):
         self.dealer += 1
 
+    def add_to_push(self):
+        self.push += 1
+
     def get_total(self):
-        return self.dealer + self.player
+        return self.dealer + self.player + self.push
 
     def get_win_percent(self):
         return get_percentage(self.player, self.get_total())
 
     def to_string(self):
-        return "player = " + str(self.player) + " dealer = " + str(self.dealer) + " total = " + str(self.get_total()) + " win% = " + str(self.get_win_percent())
+        return "player = " + str(self.player) + " dealer = " + str(self.dealer) + " push = " + str(self.push) + " total = " + str(self.get_total()) + " win% = " + str(self.get_win_percent())
